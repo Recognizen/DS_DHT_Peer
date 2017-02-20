b92824028da9e9581a6695a9aec15c60bb3166ab
recognizen <bogdanliviu_a@yahoo.com>
2017-02-20 21:18:15 +0100

    Implemented select N clockwise nodes function

--- src/ds_project/node/NodeApp.java
+++ src/ds_project/node/NodeApp.java
@@ -14,15 +14,20 @@ import scala.concurrent.duration.Duration;
 import com.typesafe.config.ConfigFactory;
 import com.typesafe.config.Config;
 import ds_project.node.Item;
+import java.util.Iterator;
+import java.util.Set;
+import java.util.TreeMap;
+import java.util.TreeSet;
 
 public class NodeApp {
     static private String remotePath = null; // Akka path of the bootstrapping peer
     static private int myId; // ID of the local node    
+    static final private int N = 2;
     	
     public static class Node extends UntypedActor {
 		
         // The table of all nodes in the system id->ref
-        private Map<Integer, ActorRef> nodes = new HashMap<>();
+        private Map<Integer, ActorRef> nodes = new TreeMap<>();
         private Map<Integer, Item> dataItems = new HashMap<>();
         
         public void preStart() {
@@ -31,11 +36,14 @@ public class NodeApp {
                // getContext().actorSelection(remotePath).tell(new GetKey(1), getSelf());
             }
             nodes.put(myId, getSelf());
-            dataItems.put(myId, new Item(myId,"test"+myId ,1));
+            nodes.put(70, getSelf());
+            nodes.put(60, getSelf());
+            dataItems.put(myId, new Item(61,"test"+myId ,1));
             
         }
 
         public void onReceive(Object message) {
+            
             if (message instanceof RequestNodelist) {
                 getSender().tell(new Nodelist(nodes), getSelf());
             }
@@ -73,6 +81,7 @@ public class NodeApp {
             else if (message instanceof GetKey){
                 //extract the keyId from the message
                 Integer itemKey = ((GetKey)message).keyId;
+                calculateNodes(itemKey);
                 //respond to the sender with the local dataItem having that key
                 getSender().tell(new DataItem(dataItems.get(itemKey)), getSelf());
             }
@@ -84,6 +93,56 @@ public class NodeApp {
             else
             	unhandled(message);		// this actor does not handle any incoming messages
         }
+        
+        //check on number of nodes should be done earlier
+        public ArrayList<Integer> calculateNodes(Integer itemKey){
+            //N replication parameter
+            ArrayList<Integer> interestedNodes = new ArrayList<>();
+            Set<Integer> keys = nodes.keySet();
+            
+            System.out.println("Printing key set "+ keys.toString());
+            
+            Iterator it = keys.iterator();
+            
+            Integer key;
+            
+            //find the N clockwise nodes
+            while(it.hasNext()){
+                key = (Integer) it.next();
+                if(key != null && key >= itemKey){
+                    interestedNodes.add(key);
+                    itemKey = key+1;
+                }
+            }
+            
+            it = keys.iterator();
+            while(interestedNodes.size()<N && it.hasNext()){
+                key = (Integer) it.next();
+                interestedNodes.add(key);
+            }
+            
+            //find the N clockwise nodes
+            /*do{
+                key = keys.ceiling(itemKey);
+                if(key != null){
+                    interestedNodes.add(key);
+                    itemKey = key+1;
+                }
+            } while(interestedNodes.size() == N || key == null);
+            
+            //need to wrap around the set until N nodes are found
+            itemKey = 0;
+            
+            while(interestedNodes.size()<N){
+                key = keys.floor(itemKey);
+                interestedNodes.add(key);
+                itemKey = key + 1;
+            }
+            */
+            System.out.println("Selected Nodes "+interestedNodes.toString());
+            return interestedNodes;
+        }
+       
     }
     
     public static class Update implements Serializable{
@@ -157,9 +216,9 @@ public class NodeApp {
         
         if(args[0].equals("node3")){
             System.out.println("Trying to send message");
-            system.actorSelection(remotePath).tell(new GetKey(1), receiver); 
-            system.actorSelection(remotePath).tell(new Update(1, "yoyo"), receiver);
-            system.actorSelection(remotePath).tell(new GetKey(1), receiver);
+            system.actorSelection(remotePath).tell(new GetKey(61), receiver); 
+            system.actorSelection(remotePath).tell(new Update(61, "yoyo"), receiver);
+            system.actorSelection(remotePath).tell(new GetKey(61), receiver);
         }
     }
 }
