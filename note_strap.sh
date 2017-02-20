e637162f9652c7fdbe7c9913f18a243672f1ac3a
recognizen <bogdanliviu_a@yahoo.com>
2017-02-20 21:19:18 +0100

    Script to put up a network

---
+++ note_strap.sh
@@ -0,0 +1,7 @@
+#!/bin/bash
+
+java -cp $AKKA_CLASSPATH:.:/home/recognition/Desktop/CourseWork/DS1/DS_Project/build/classes/ NodeApp first &
+cd config_files/
+for f in *; do
+    java -cp $AKKA_CLASSPATH:.:/home/recognition/Desktop/CourseWork/DS1/DS_Project/build/classes/ NodeApp "${f%.*}"  127.0.0.1 10001 &
+done
