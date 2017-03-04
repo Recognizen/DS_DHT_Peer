3ac32cfbbcb6a4fa889aefb307b2b80aa354d0a1
recognizen <bogdanliviu_a@yahoo.com>
2017-03-04 19:03:34 +0100

    Adapted .conf files for Remoting Artery

--- node_start.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/bash
-
-java -cp $AKKA_CLASSPATH:.:/home/recognition/Desktop/CourseWork/DS1/DS_Project/build/classes/ds_project/node NodeApp first &
-#cd config_files/
-#for f in *; do
-#    java -cp $AKKA_CLASSPATH:.:/home/recognition/Desktop/CourseWork/DS1/DS_Project/build/classes/ NodeApp "${f%.*}"  127.0.0.1 10001 &
-#done
